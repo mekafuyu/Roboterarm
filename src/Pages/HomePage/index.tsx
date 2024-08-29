b1a6a7cb8a31b49f7390730dd3796ef64a75b389
mekafuyu <mayconbrunob@hotmail.com>
2024-08-29 11:13:08 -0300

    starting home page

---
+++ src/Pages/HomePage/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.scss";
+
+export default function HomePage(){
+
+    return (
+        <>
+        <div>
+            <div className={styles.rectangle} id="rectangle_6"></div>
+            <div className={styles.rectangle} id="rectangle_5"></div>
+            <div className={styles.rectangle} id="rectangle_4"></div>
+            <div className={styles.rectangle} id="rectangle_3"></div>
+            <div className={styles.rectangle} id="rectangle_2"></div>
+            <div className={styles.rectangle} id="rectangle_1"></div>
+        </div>
+        </>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App.tsx'
 import './index.css'
+import HomePage from './Pages/HomePage/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/home",
+    element: <HomePage/>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
