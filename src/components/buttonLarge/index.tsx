e4c4dee8b237b646e4f2bf553c55fc102ebedec6
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-09-03 11:02:05 -0300

    Home resposivity

--- src/components/buttonLarge/index.tsx
+++ src/components/buttonLarge/index.tsx
@@ -2,10 +2,10 @@ import styles from "./styles.module.scss";
 
 import Button from "@mui/material/Button";
 
-export default function ButtonLarge() {
+export default function ButtonLarge({ text }: { text: string }) {
     return (
         <div>
-            <Button variant="contained" className={styles.size} >Contained</Button>
+            <Button variant="contained" className={styles.size}>{text}</Button>
         </div>
     );
 }
--- src/components/themes/index.tsx
+++ src/components/themes/index.tsx
@@ -1,4 +1,6 @@
 import { createTheme } from '@mui/material/styles';
+import styles from "./styles.module.scss";
+
 
 const ligth = createTheme({
   palette: {
@@ -13,6 +15,48 @@ const ligth = createTheme({
       main: '#EFEFEF',
       dark: '#ba000d',
       contrastText: '#000',
+    },
+  },  
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        h1: {
+            fontSize: "60px",
+            fontFamily: 'Inter, sans-serif',
+            fontOpticalSizing: 'auto',
+            fontWeight: 600,
+            fontStyle: 'normal',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "4%",
+        },
+        h2: {
+            fontSize: "32px",
+            fontFamily: 'Inter, sans-serif',
+            fontOpticalSizing: 'auto',
+            fontWeight: 600,
+            fontStyle: 'normal',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "4%",
+        },
+    },
+      defaultProps: {
+        variantMapping: {
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h2',
+          h4: 'h2',
+          h5: 'h2',
+          h6: 'h2',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'span',
+          body2: 'span',
+        },
+      },
     },
   },
 });
