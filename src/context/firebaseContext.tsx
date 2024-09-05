1dc608ff969c4a65f54434ba31f4f80fb9c9db66
mekafuyu <mayconbrunob@hotmail.com>
2024-09-05 10:56:28 -0300

    :sparkles: firebase context

---
+++ src/context/firebaseContext.tsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  };
+
+const fb_app = initializeApp(firebaseConfig);
+
+const firebaseContext = createContext(fb_app);
+
+export default firebaseContext;
